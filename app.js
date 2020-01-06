fad7ef4824ca1e3fd68e469ff84e749884d32b8f
Pouya Oftadeh <poftadeh2@gmail.com>
2020-01-06 15:33:47 -0500

    feat: add saving submitted data to zip

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
 const multer = require('multer');
 const moment = require('moment');
-const { execSync } = require('child_process');
+const AdmZip = require('adm-zip');
 
-const storage = multer.diskStorage({
-  destination: './uploads',
-  filename: (req, file, cb) => cb(null, file.originalname),
-});
-
-const upload = multer({ storage });
+const app = express();
+const zip = new AdmZip();
+const upload = multer();
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -23,16 +18,27 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => res.sendFile('index.html'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 app.post('/upload', upload.single('upload-pdf'), (req, res) => {
   console.log('submitted!', req.file, JSON.stringify(req.body));
-  res.json(req.body);
+  zip.addFile(req.file.originalname, req.file.buffer);
+  zip.addFile(
+    'options.txt',
+    `Transition=${req.body.transition}\nAdvance=${req.body.interval}`,
+  );
+  zip.writeZip(
+    `./archives/sign${req.body.signNumber}/${moment().format(
+      'YYYYMMDDhhmmss',
+    )}.zip`,
+  );
+  res.sendStatus(200);
 });
 
 app.use((err, req, res, next) => {
   const { statusCode, status } = err;
-
   res.status(statusCode || 500).json({
     status: status || 'error',
     message: err.message,
