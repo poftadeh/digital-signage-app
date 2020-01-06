9491b67c7acd5e67c89e970eef8cf0704a760366
Pouya Oftadeh <poftadeh2@gmail.com>
2020-01-06 11:23:45 -0500

    feat: add express body parser

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log(
+    `Method: '${req.method}', URL: ${req.originalUrl}, Body: ${JSON.stringify(
+      req.body,
+    )}`,
+  );
+  next();
+});
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.post('/', (req, res) => res.statusCode(404).send('nothing here'));
+app.post('/upload', (req, res) => {
+  console.log('submitted!');
+  res.send('done');
+});
 
 app.use((err, req, res, next) => {
-  // console.log(err.stack);
-
   const { statusCode, status } = err;
 
   res.status(statusCode || 500).json({
