3a7d503348b8aadc51e187c3d481589fb70625c8
Pouya Oftadeh <poftadeh2@gmail.com>
2020-01-08 14:30:08 -0500

    refactor: change default port to 8888

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () =>
   console.log(`digital signage server is listening on port ${PORT}`),
